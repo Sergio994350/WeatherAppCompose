fdd1c3fe11d87086229c21fb57001b7305960b13
Sergey Tarasov <tarasov-mnm@yandex.ru>
2022-07-05 10:47:10 +0300

    add mappers & data classes

---
+++ app/src/main/java/com/sergio994350/weatherappcompose/data/mappers/WeatherMappers.kt
@@ -0,0 +1,55 @@
+package com.sergio994350.weatherappcompose.data.mappers
+
+import android.annotation.SuppressLint
+import com.sergio994350.weatherappcompose.data.remote.WeatherDataDto
+import com.sergio994350.weatherappcompose.data.remote.WeatherDto
+import com.sergio994350.weatherappcompose.domain.weather.WeatherData
+import com.sergio994350.weatherappcompose.domain.weather.WeatherInfo
+import com.sergio994350.weatherappcompose.domain.weather.WeatherType
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+data class IndexedWeatherData(
+    val index: Int,
+    val data: WeatherData
+)
+
+@SuppressLint("NewApi")
+fun WeatherDataDto.toWeatherDataMap(): Map<Int, List<WeatherData>> {
+    return time.mapIndexed { index, time ->
+        val temperature = temperatures[index]
+        val weatherCode = weatherCodes[index]
+        val windSpeed = windSpeeds[index]
+        val pressure = pressures[index]
+        val humidity = humidities[index]
+        IndexedWeatherData(
+            index = index,
+            data = WeatherData(
+                time = LocalDateTime.parse(time, DateTimeFormatter.ISO_DATE_TIME),
+                temperatureCelsius = temperature,
+                pressure = pressure,
+                windSpeed = windSpeed,
+                humidity = humidity,
+                weatherType = WeatherType.fromWMO(weatherCode)
+            )
+        )
+    }.groupBy {
+        it.index / 24
+    }.mapValues {
+        it.value.map { it.data }
+    }
+}
+
+@SuppressLint("NewApi")
+fun WeatherDto.toWeatherInfo(): WeatherInfo {
+    val weatherDataMap = weatherData.toWeatherDataMap()
+    val now = LocalDateTime.now()
+    val currentWeatherData = weatherDataMap[0]?.find {
+        val hour = if (now.minute < 30) now.hour else now.hour + 1
+        it.time.hour == hour
+    }
+    return WeatherInfo(
+        weatherDataPerDay = weatherDataMap,
+        currentWeatherData = currentWeatherData
+    )
+}
