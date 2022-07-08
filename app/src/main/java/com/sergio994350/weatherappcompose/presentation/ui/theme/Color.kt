41a211448ea4937c600f0641d2e52e6fa5a28b3b
Sergey Tarasov <tarasov-mnm@yandex.ru>
2022-07-08 10:57:02 +0300

    app version 0.8

--- app/src/main/java/com/sergio994350/weatherappcompose/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/sergio994350/weatherappcompose/presentation/ui/theme/Color.kt
@@ -2,6 +2,9 @@ package com.sergio994350.weatherappcompose.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val DarkBlue = Color(0xFF1B3B5A)
+val DeepBlue = Color(0xFF102840)
+
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
